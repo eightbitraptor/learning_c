8c2ae09dc2c5ac73029ed02ffb960e6ec0468c29
Matt Valentine-House <matt@eightbitraptor.com>
2020-09-22 15:32:09 +0100

    pull the array and the length together into a type

--- arrays.c
+++ arrays.c
@@ -3,13 +3,30 @@
 
 #define ARRLEN 10
 
+typedef struct arr_t {
+    char * data;
+    int len;
+} arr_t;
+
+arr_t *
+new_arr(int len)
+{
+    char *c = calloc(len, sizeof(char));
+    arr_t *arr = malloc(sizeof(arr_t));
+
+    arr->data = c;
+    arr->len = 0;
+
+    return arr;
+}
+
 int
-simple_find(char * arr, int len, char elem)
+simple_find(arr_t * arr, int len, char elem)
 {
     int found_idx = 0;
 
     for( int i = 0; i < len; i++) {
-        if( arr[i] == elem){
+        if( arr->data[i] == elem){
             found_idx = i;
             break;
         }
@@ -19,44 +36,44 @@ simple_find(char * arr, int len, char elem)
 }
 
 int
-binary_search(char * arr, int start, int end, char elem)
+binary_search(arr_t * arr, int start, int end, char elem)
 {
     int mid = start + (end - start) / 2;
 
     // found it
-    if(arr[mid] == elem) return mid;
+    if(arr->data[mid] == elem) return mid;
     // if the mid point is larger than array then must be in left
-    if(arr[mid] > elem) return binary_search(arr, 0, mid - 1, elem);
+    if(arr->data[mid] > elem) return binary_search(arr, 0, mid - 1, elem);
     // otherwise it must be in the right
     return binary_search(arr, mid + 1, end, elem);
 }
 
 int
-partition(char * arr, int low, int high)
+partition(arr_t * arr, int low, int high)
 {
-    char pivot = arr[high];
+    char pivot = arr->data[high];
     int i = (low - 1);
     int temp = 0;
 
     for(int j = low; j <= high - 1; j++)
     {
-        if(arr[j] < pivot) {
+        if(arr->data[j] < pivot) {
             i++;
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            temp = arr->data[i];
+            arr->data[i] = arr->data[j];
+            arr->data[j] = temp;
         }
     }
 
-    temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
+    temp = arr->data[i + 1];
+    arr->data[i + 1] = arr->data[high];
+    arr->data[high] = temp;
 
     return(i + 1);
 }
 
 void
-quicksort(char * arr, int low, int high)
+quicksort(arr_t * arr, int low, int high)
 {
     if (low < high) {
         int pivot = partition(arr, low, high);
@@ -67,27 +84,28 @@ quicksort(char * arr, int low, int high)
 }
 
 void
-push_char_at(char * arr, int *size, int loc, char elem)
+push_char_at(arr_t * arr, int loc, char elem)
 {
     // make space for the new element
-    arr = realloc(arr, *size + 1);
-    *size = *size + 1;
+    arr->data = realloc(arr->data, arr->len + 1);
+    arr->len = arr->len + 1;
+
     // move everything after the chosen location out of the way
-    for (int i = *size-1; i >= loc; i--) {
-        arr[i + 1] = arr[i];
+    for (int i = arr->len - 1; i >= loc; i--) {
+        arr->data[i + 1] = arr->data[i];
     }
     // push the elem in to the arr
-    arr[loc] = elem;
+    arr->data[loc] = elem;
 }
 
 void
-append(char * arr, int *size, char elem) {
-    push_char_at(arr, size, *size, elem);
+append(arr_t * arr, char elem) {
+    push_char_at(arr, arr->len, elem);
 }
 
 void
-prepend(char * arr, int *size, char elem) {
-    push_char_at(arr, size, 0, elem);
+prepend(arr_t * arr, char elem) {
+    push_char_at(arr, 0, elem);
 }
 
 int
@@ -95,16 +113,15 @@ main()
 {
     char *str = "abcdefghijklmnopqrstuvwxyz";
 
-    char *chars = calloc(ARRLEN, sizeof(char));
-    int size = 0;
+    arr_t * chars = new_arr(ARRLEN);
 
     for( int i = 0; i < ARRLEN; i++) {
-        chars[i] = str[i];
-        size++;
+        chars->data[i] = str[i];
+        chars->len++;
     }
 
     for( int i = 0; i < ARRLEN; i++)
-        printf("%c", chars[i]);
+        printf("%c", chars->data[i]);
 
     printf("\n");
     printf("found the letter b at %i using scanning\n", simple_find(chars, ARRLEN, 'b'));
@@ -114,15 +131,18 @@ main()
         printf("found the letter %c at %i\n",c , binary_search(chars, 0, ARRLEN - 1, c));
     }
 
-    push_char_at(chars, &size, 5, 'Z');
-    append(chars, &size, 'A');
-    prepend(chars, &size, 'X');
-    printf("%s\n", chars);
+    printf("%s\n", chars->data);
+    push_char_at(chars, 5, 'Z');
+    printf("%s\n", chars->data);
+    append(chars, 'A');
+    printf("%s\n", chars->data);
+    prepend(chars, 'X');
+    printf("%s\n", chars->data);
 
 
     printf("sorting...\n");
-    quicksort(chars, 0, size - 1);
-    printf("%s\n", chars);
+    quicksort(chars, 0, chars->len - 1);
+    printf("%s\n", chars->data);
 
     free(chars);
 }
